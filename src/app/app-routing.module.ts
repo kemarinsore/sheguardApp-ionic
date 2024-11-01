95c7c32efc8731d8e801fd6b309871f2f820ade4
Ikhwan Alfi <ikhwanalfi228@gmail.com>
2024-11-01 13:51:12 +0700

    login, panicBotton, Notifikasi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,37 +4,50 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () =>
+      import('./pages/tabs/tabs.module').then((m) => m.TabsPageModule),
   },
   {
     path: 'intro',
-    loadChildren: () => import('./pages/intro/intro.module').then( m => m.IntroPageModule)
+    loadChildren: () =>
+      import('./pages/intro/intro.module').then((m) => m.IntroPageModule),
   },
   {
     path: 'set-up',
-    loadChildren: () => import('./pages/set-up/set-up.module').then( m => m.SetUpPageModule)
+    loadChildren: () =>
+      import('./pages/set-up/set-up.module').then((m) => m.SetUpPageModule),
   },
   {
     path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () =>
+      import('./pages/login/login.module').then((m) => m.LoginPageModule),
   },
   {
     path: 'forgot-password',
-    loadChildren: () => import('./pages/forgot-password/forgot-password.module').then( m => m.ForgotPasswordPageModule)
+    loadChildren: () =>
+      import('./pages/forgot-password/forgot-password.module').then(
+        (m) => m.ForgotPasswordPageModule
+      ),
   },
   {
     path: 'verifikasi-email',
-    loadChildren: () => import('./pages/verifikasi-email/verifikasi-email.module').then( m => m.VerifikasiEmailPageModule)
+    loadChildren: () =>
+      import('./pages/verifikasi-email/verifikasi-email.module').then(
+        (m) => m.VerifikasiEmailPageModule
+      ),
   },
   {
     path: 'sandi-baru',
-    loadChildren: () => import('./pages/sandi-baru/sandi-baru.module').then( m => m.SandiBaruPageModule)
+    loadChildren: () =>
+      import('./pages/sandi-baru/sandi-baru.module').then(
+        (m) => m.SandiBaruPageModule
+      ),
   },
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
